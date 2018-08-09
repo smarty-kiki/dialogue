c8600fb27d386cfdea28a308362a6804e7f59397
yaoyang <kiki@smarty.so>
2018-08-09 21:24:53 +0800

    after push 添加构建

--- project/tool/after_push.sh
+++ project/tool/after_push.sh
@@ -5,6 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../..
 ln -fs $ROOT_DIR/project/config/production/nginx/dialogue.conf /etc/nginx/sites-enabled/dialogue
 /usr/sbin/service nginx reload
 
+/bin/bash $ROOT_DIR/project/tool/dep_build.sh link
 /usr/bin/php $ROOT_DIR/public/cli.php migrate
 
 ln -fs $ROOT_DIR/project/config/production/supervisor/dialogue_operator.conf /etc/supervisor/conf.d/dialogue_operator.conf
