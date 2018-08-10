daff97c9f52bb44f97cd21f24986607f1311197f
yaoyang <kiki@smarty.so>
2018-08-10 10:28:55 +0800

    修改 ROOT_DIR 路径获取方式、添加 mysql 线上配置

--- project/tool/after_push.sh
+++ project/tool/after_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../..
+ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../..
 
 ln -fs $ROOT_DIR/project/config/production/nginx/dialogue.conf /etc/nginx/sites-enabled/dialogue
 /usr/sbin/service nginx reload
