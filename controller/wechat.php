08b96ccef97203fb14468b1972b932745c85108c
yaoyang <kiki@smarty.so>
2018-08-15 13:28:35 +0800

    添加 wechat 消息响应 controller 到 index

--- controller/wechat.php
+++ controller/wechat.php
@@ -2,9 +2,9 @@
 
 if_get('/wechat/receive', function ()
 {
-    list($msg_signature, $timestamp, $nonce, $echostr) = input_list('msg_signature', 'timestamp', 'nonce', 'echostr');
+    list($signature, $timestamp, $nonce, $echostr) = input_list('signature', 'timestamp', 'nonce', 'echostr');
 
-    return wechat_verify_url($msg_signature, $timestamp, $nonce, $echostr);
+    return wechat_verify_url($signature, $timestamp, $nonce, $echostr);
 });
 
 if_post('/wechat/receive', function ()
