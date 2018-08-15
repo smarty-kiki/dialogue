3b129734ff67ad26159cbfea4197b964eef423e3
yaoyang <kiki@smarty.so>
2018-08-15 13:32:29 +0800

    debug token 校验失败的问题

--- util/wechat/wechat.php
+++ util/wechat/wechat.php
@@ -14,11 +14,24 @@ function wechat_verify_url($msg_signature, $timestamp, $nonce, $echostr)
         throw new Exception('IllegalAesKey');
     }
 
-    $signature = business_wechat_sha1($token, $timestamp, $nonce, $echostr);
+    $signature = wechat_sha1($token, $timestamp, $nonce);
 
     if ($signature != $msg_signature) {
         throw new Exception('ValidateSignatureError');
     }
 
-    return $signature;
+    return $echostr;
+}/*}}}*/
+
+function wechat_sha1($token, $timestamp, $nonce)
+{/*{{{*/
+    try {
+        $array = array($token, $timestamp, $nonce);
+        sort($array, SORT_STRING);
+        $str = implode($array);
+
+        return sha1($str);
+    } catch (Exception $e) {
+        throw new Exception('ComputeSignatureError');;
+    }
 }/*}}}*/
