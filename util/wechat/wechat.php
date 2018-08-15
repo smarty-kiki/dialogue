eacf6655edcd2baec9b0e3d7e2b3576d8ef1bf28
yaoyang <kiki@smarty.so>
2018-08-16 01:13:01 +0800

    支持回复用户时显示输入中状态

--- util/wechat/wechat.php
+++ util/wechat/wechat.php
@@ -1,7 +1,7 @@
 <?php
 
 define('WECHAT_ID', 'pagoda_tech');
-define('WECHAT_APP_ID', 'AppIDwx72dd707ef1edfa1a');
+define('WECHAT_APP_ID', 'wx72dd707ef1edfa1a');
 define('WECHAT_APP_SECRET', 'd2ee36a4a0c2f82954894a36c0a4b031');
 define('WECHAT_TOKEN', 'HelloWorld');
 define('WECHAT_ENCODING_AES_KEY', '0o9iORmjwPszqCzjZA5dqO4aA4nif2OmeyKnjBvPeFd');
