5143eaba1755deaf3411f097e6c8c788f1f2575d
yaoyang <kiki@smarty.so>
2018-10-24 17:12:00 +0800

    添加测试正则的对话

--- command/dialogue_topic/load.php
+++ command/dialogue_topic/load.php
@@ -1,3 +1,4 @@
 <?php
 
 include DIALOGUE_TOPIC_DIR.'/douban_book.php';
+include DIALOGUE_TOPIC_DIR.'/regexp.php';
---
+++ command/dialogue_topic/regexp.php
@@ -0,0 +1,40 @@
+<?php
+
+dialogue_topic([
+    '测试正则',
+], function ($user_id, $content, $time) {/*{{{*/
+
+    $pattern = $subject = null;
+
+    $ask = '好，发我吧';
+
+    $is_over_closure = function ($answer) {
+        return ! array_search($answer, ['谢了', '谢谢', '完事', '好了', '好的']);
+    };
+
+    while ($answer = dialogue_ask_and_wait($user_id, $ask, $is_over_closure, 120)) {
+
+        if (starts_with($answer, '/')) {
+            $pattern = $answer;
+        } else {
+            $subject = $answer;
+        }
+
+        if (is_null($pattern)) {
+            $ask = '收到，发我一下正则吧';
+        } elseif (is_null($subject)) {
+            $ask = '收到，发我一下测试值吧';
+        } else {
+            $matches = [];
+            if (false === preg_match_all($pattern, $subject, $matches)) {
+                $ask = '正则['.$pattern.']有问题';
+            } else {
+                ob_start();
+                var_dump($matches);
+                $ask = ob_get_contents();
+                ob_end_clean();
+            }
+        }
+    }
+
+});/*}}}*/
