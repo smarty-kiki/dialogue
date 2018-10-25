f67e94ac5564cc71c73f31bcd761b8a22e1b523f
yaoyang <kiki@smarty.so>
2018-10-25 22:22:38 +0800

    修改 textrank 逻辑为阿里云 API

--- util/nlp/text_rank.php
+++ util/nlp/text_rank.php
@@ -1,18 +1,15 @@
 <?php
 
+define('NLP_APP_KEY', '25224800');
+define('NLP_APP_SECRET', '29ef121a79bae9c762e8626d6a0c856f');
+define('NLP_APP_CODE', 'ca42dc926d8a498db18926928a3e9f2a');
+
 function text_rank_get_summary($text)
 {/*{{{*/
-    $res_html = remote_post('https://cws.9sep.org/textrank', http_build_query([
-        'text' => $text,
-        'topk' => 3,
-    ]), 5, 3);
+    $res = remote_post_json('http://summary.market.alicloudapi.com/clouds/nlp/summary', json([
+        'document' => $text,
+        'maxLength' => mb_strlen($text) * 0.2,
+    ]), 10, 3, ['Authorization:APPCODE '.NLP_APP_CODE]);
 
-    if ($res_html) {
-
-        $res_html = mb_convert_encoding($res_html, 'utf-8');
-
-        $dom = str_get_html($res_html);
-
-        return $dom->find('[name=excerpt]')[0]->innertext;
-    }
+    return $res['data'];
 }/*}}}*/
