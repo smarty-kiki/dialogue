2412da0275cb2fb2aca7af92f93530af093aaee2
yaoyang <kiki@smarty.so>
2019-01-04 14:15:34 +0800

    debug

--- util/slack/slack.php
+++ util/slack/slack.php
@@ -2,7 +2,7 @@
 
 function slack_say_to_smarty_ds($message)
 {
-    return remote_post('https://hooks.slack.com/services/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', json_encode([
+    return remote_post('https://hooks.slack.com/services/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', json_encode([
         'text' => $message
     ]), 10);
 }
