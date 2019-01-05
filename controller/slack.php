b2227e6e4ea834446e3125b9c91bf920ea061e88
yaoyang <kiki@smarty.so>
2019-01-05 14:12:59 +0800

    改为依赖 app_mention 事件响应回复

--- controller/slack.php
+++ controller/slack.php
@@ -21,7 +21,7 @@ if_post('/slack/event', function ()
         /**kiki*/error_log(strip_tags(print_r($event, true))."\n", 3, "/tmp/error_user.log");
 
         switch ($event['type']) {
-            case 'message':
+            case 'app_mention':
 
                 $reply_message = dialogue_push('slack_smarty_coin', $event['text']);
 
