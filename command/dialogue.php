f5dca25d28cd0faecae7bbae6814aedf463f63e0
yaoyang <kiki@smarty.so>
2019-01-05 14:33:37 +0800

    调整 @ 别人时候的格式

--- command/dialogue.php
+++ command/dialogue.php
@@ -10,7 +10,7 @@ command('dialogue:operator', '启动接线员', function ()
     dialogue_send_action(function ($user_id, $message) {
         //business_wechat_send_message($user_id, [],[],$message);
 
-        slack_say_to_smarty_coin("<@$user_id>".$message);
+        slack_say_to_smarty_coin("<@$user_id> ".$message);
     });
 
     dialogue_topic_miss_action(function ($user_id, $message) {
