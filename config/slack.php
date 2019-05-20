a17448472d1899cddcadca212516ff7abc6848e3
yaoyang <kiki@smarty.so>
2019-05-20 19:22:26 +0800

    debug

--- config/slack.php
+++ config/slack.php
@@ -2,5 +2,5 @@
 
 return [
 
-    'bot_token' => 'xoxb-120345614548-628823563699-TpX6kEoYief4IVAGUuwt6FnJ',
+    'bot_token_base64encode' => 'eG94Yi0xMjAzNDU2MTQ1NDgtNjI4ODIzNTYzNjk5LWhnc2lOZzhuaUN0eWdGcW9RbmxBNjA3WQ==',
 ];
