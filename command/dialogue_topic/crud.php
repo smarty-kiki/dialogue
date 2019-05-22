f1a263889a45cbdf5a1fff52f4002f097002c75f
yaoyang <kiki@smarty.so>
2019-05-22 16:56:04 +0800

    添加 CRUD demo

--- command/dialogue_topic/crud.php
+++ command/dialogue_topic/crud.php
@@ -52,58 +52,58 @@ dialogue_topic([
 
         // 枚举识别
         if (
-            str_finish($column_name, '分类') || 
-            str_finish($column_name, '类型') || 
-            str_finish($column_name, '状态') || 
-            str_finish($column_name, '阶段') || 
-            str_finish($column_name, '种类')
+            ends_with($column_name, '分类') ||
+            ends_with($column_name, '类型') ||
+            ends_with($column_name, '状态') ||
+            ends_with($column_name, '阶段') ||
+            ends_with($column_name, '种类')
         ) {
             $message = dialogue_ask_and_wait($user_info, '好的，'.$column_name.'是枚举吧？有哪些呢？一次发给我吧');
             if (! $message) return;
             $answer_str = $message['content'];
 
-            if (! str_finish($answer_str, '不是')) {
+            if (! ends_with($answer_str, '不是')) {
                 $entity_struct['format'] = split_chinese_words($answer_str);
             }
         }
 
         // 价格识别
         if (
-            str_finish($column_name, '价格') || 
-            str_finish($column_name, '价钱') || 
-            str_finish($column_name, '金额') || 
-            str_finish($column_name, '价') || 
-            str_finish($column_name, '金') || 
-            str_finish($column_name, '价值')
+            ends_with($column_name, '价格') ||
+            ends_with($column_name, '价钱') ||
+            ends_with($column_name, '金额') ||
+            ends_with($column_name, '价') ||
+            ends_with($column_name, '金') ||
+            ends_with($column_name, '价值')
         ) {
             $message = dialogue_ask_and_wait($user_info, '好的，'.$column_name.'是金钱单位吧？');
             if (! $message) return;
             $answer_str = $message['content'];
 
-            if (! str_finish($answer_str, '不是')) {
+            if (! ends_with($answer_str, '不是')) {
                 $entity_struct['datatype'] = 'int(11)';
             }
         }
 
         // 时间识别
         if (
-            str_finish($column_name, '日') || 
-            str_finish($column_name, '日期') || 
-            str_finish($column_name, '时间') || 
-            str_finish($column_name, '时间点')
+            ends_with($column_name, '日') ||
+            ends_with($column_name, '日期') ||
+            ends_with($column_name, '时间') ||
+            ends_with($column_name, '时间点')
         ) {
             $message = dialogue_ask_and_wait($user_info, '好的，'.$column_name.'是日期 (精确到日的) 还是时间 (精确到秒的)？');
             if (! $message) return;
             $answer_str = $message['content'];
 
             if (
-                str_finish($answer_str, '时间')
+                ends_with($answer_str, '时间')
             ) {
                 $entity_struct['datatype'] = 'datetime';
             }
 
             if (
-                str_finish($answer_str, '日期')
+                ends_with($answer_str, '日期')
             ) {
                 $entity_struct['datatype'] = 'date';
             }
